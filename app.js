07f0e2a00d932f65e51afa6bc532770ce6f9a7d4
Ishaan Sharma <ishaanshama93@gmail.com>
2024-11-02 20:34:26 +0530

    adding colors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let h1 = document.querySelector("h1");
 h1.addEventListener("onmouseenter",() => {
     h1.style.color = "yellow";
-    h1.style.backgroundColorcolor = "blue";
+    h1.style.backgroundColorcolor = "blue";//adding colors 
 });
